2718f8b5148f4ad27a91e6586f17324074b44583
BirjeesAli <birjees.zenkoders@gmail.com>
2023-07-31 23:06:50 -0700

    spinning fixed.

--- spin.js
+++ spin.js
@@ -1,21 +1,23 @@
 function reverse(s){
     const size = (s.length)-1;
     let sub = '';
-    for (let n = 0; n <= Math.ceil(size/2); n++){
+    for (let n = 0; n <= size; n++){
         sub = s[n]+sub;
     }
-    return (s.slice(Math.ceil(size/2)+1)+sub);
+    return (sub);
 }
 
 function spinWords(string){
     let res = '';
-    let size = string.split(' ');
-    for (let i=0; i<size; i++){
-        console.log(i)
+    let arr = string.split(' ');
+    for (let i=0; i<arr.length; i++){
+        if (arr[i].length > 4){
+            res+=reverse(arr[i]);
+        }
+        else res+=arr[i];
+        if (i < arr.length-1) res+=' ';
     }
+    return res;
   }
 
-
-
-// console.log(reverse('1234'))
 console.log(spinWords("Hey fellow warriors"));
