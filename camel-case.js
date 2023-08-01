25a9ebd0500729cb77bf6009699c046a6ec9c3d5
Birjees <birjees.zenkoders@gmailcom>
2023-08-01 14:30:06 +0500

    Reduced Complexity

--- camel-case.js
+++ camel-case.js
@@ -1,30 +1,11 @@
 function toCamelCase(str){
-    if (str === '') return str;
-    function splitter(s){
-        if (s.includes('-')) return s.split('-');
-        else if (s.includes('_')) return s.split('_');
-        return false;
-    }
-    if(!(splitter(str))){
-        return((97 >= str[0].charCodeAt() <= 122 ? str[0].toUpperCase()+str.slice(1) : str) )
-    }
-
-    str = splitter(str);
-    let res = '';
-    for (const word of str){
-        if (splitter(word)) {
-            let words = splitter(word);
-            let flag = word == str[0];
-            for (const word of words){
-                res += ((flag ? (word != words[0]) : true) && (97 >= word[0].charCodeAt() <= 122) ? word[0].toUpperCase()+word.slice(1) : word);
-            }
-            continue;
-        } 
-        res += ((word != str[0]) && (97 >= word[0].charCodeAt() <= 122) ? word[0].toUpperCase()+word.slice(1) : word);
-    }
-    return res
+    str = str.split(/-|_/);
+    if(str.length < 2) return(str[0]);
+    return (str.map((item,idx) => (idx > 0 ? item[0].toUpperCase()+item.slice(1,) : item )).join(''));
 }
 
 console.log(toCamelCase("a_Cat_Is-Omoshiroi")); // aCatIsOmoshiroi
 console.log(toCamelCase("The-pippi_Is_Hungry")); // ThePippiIsHungry
 console.log(toCamelCase("a-cat-Is_cute")); //aCatIsCute
+console.log(toCamelCase("")); // ''
+console.log(toCamelCase("cute")); //cute
